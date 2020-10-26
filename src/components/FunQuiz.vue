8ee42060ad3f9ac5ad75daa6de507c4902ca4a52
dipsaus <d.changur@live.nl>
2020-10-26 22:15:16 +0100

    quiz improvements

--- src/components/FunQuiz.vue
+++ src/components/FunQuiz.vue
@@ -15,24 +15,24 @@
 
                 <div class="jumbotron bg-light" >
 
-                    <div class="row mt-3" v-show="startQuiz || score > 0">
+                    <div class="row mt-3" v-show="startQuiz || score >= 0">
                         <div class="col-md-12" >
-                            <p class="text-primary" v-if="score >= 55 && questionIndex === quiz.questions.length">
+                            <p class="text-primary  animatedEnd animatedFadeInUp fadeInUp" v-if="score >= 55 && questionIndex === quiz.questions.length">
                                 Looks like we should grab a cup of coffee! <br> Just call me. My treat!<br>
-                                 <i class="fa fa-smile fa-5x text-primary mt-2"></i>
+                                 <i class=" animatedEnd animatedFadeInUp fadeInUp fa fa-smile fa-5x text-primary mt-2"></i>
                                 
                             </p>
-                            <p class="text-danger" v-if="score < 55 && questionIndex === quiz.questions.length">
+                            <p class="text-danger animatedEnd animatedFadeInUp fadeInUp" v-if="score < 55 && questionIndex === quiz.questions.length">
                                 Looks like we're not really a match eh..?<br>
                                 But you may still contact me, maybe we could work something out?<br>
-                                <i class="fa fa-frown fa-5x text-danger mt-2"></i>
+                                <i class=" animatedEnd animatedFadeInUp fadeInUp fa fa-frown fa-5x text-danger mt-2"></i>
                             </p>
                         </div>
 
                         
                     </div>
 
-                    <button v-show="!startQuiz" class="btn btn-lg btn-primary" v-on:click="start">Start quiz</button>
+                    <button v-show="!startQuiz" class=" animatedEnd animatedFadeInUp fadeInUp btn btn-lg btn-primary" v-on:click="start">Find out if we are a match</button>
 
                     <div class="" v-for="(question, index) in quiz.questions" :key="index">
                         <div class="row" v-show="index === questionIndex && startQuiz"> 
@@ -41,14 +41,29 @@
                                     <h3 class="text-primary mb-2" >{{question.text}}</h3>
                                 </div>
                             </div>
-                            <div class="mb-5" v-for="response in question.responses" :key="response" v-bind:class="{ 'col-md-6': question.responses.length <= 2, 'col-md-4': question.responses.length >= 3 }" >
-                                <button v-bind:disabled="clicked" v-bind:value="response.correct" v-bind:name="index" class="col-md-12 btn btn-lg" v-bind:class="{ [btnDanger] : !response.correct && clicked, [btnSucess] : response.correct  && clicked, [btnOutline] : !clicked}" v-on:click="checkAnswer(response.correct)">
-                                    {{response.text}}   <i v-bind:class="compClasses" ></i>
-                                </button>
+                            <div class="mb-5" v-for="(response, index) in question.responses" :key="response" v-bind:class="{ 'col-md-6': question.responses.length <= 2, 'col-md-4': question.responses.length >= 3 }" >
+
+                               <div class="mb-5 mr-4">
+                                    <div class="animated animatedFadeInUp fadeInUp" v-show="response.correct && clicked && (UserAnswer === response.index)">
+                                        <i class="fa fa-smile fa-2x text-success text-center absolute"></i>
+                                    </div>
+                                    <div class="animated animatedFadeInUp fadeInUp" v-show="!response.correct && clicked && (UserAnswer === response.index)">
+                                        <i class="fa fa-frown fa-2x text-danger  text-center absolute"></i>
+                                    </div>
+                               </div>
+
+                               <div class="">
+                                    <button v-bind:disabled="clicked" v-bind:value="response.correct" class="col-md-12 btn btn-lg" 
+                                        v-bind:class="{ [btnDanger] : !response.correct && clicked && (UserAnswer === response.index), [btnSucess] : response.correct  && clicked && (UserAnswer === response.index), [btnOutline] : !clicked || UserAnswer != response.index}" v-on:click="checkAnswer(response.correct, index)">
+                                        {{response.text}}   
+                                    </button>
+                               </div>
+
                             </div>
-                            <div class="col-md-12 mb-2">
+                            <div v-if="clicked" class="col-md-12 mb-2">
                                 <button class="btn btn-primary" v-bind:disabled="!clicked" v-on:click="next">
-                                    next question
+                                   <span v-if="questionIndex === quiz.questions.length - 1"> See results </span>
+                                   <span v-else> Next question </span>
                                 </button>
                             </div>
                         </div>
@@ -69,39 +84,39 @@ var quiz = {
     {
       text: "Are you looking for a developer?",
       responses: [
-        {text: 'Hell yeah', correct: true}, 
-        {text: 'Not sure', correct: true}, 
-        {text: 'Nope', correct: false}, 
+        {text: 'Hell yeah', correct: true, index: 0}, 
+        {text: 'Not sure', correct: true, index: 1}, 
+        {text: 'Nope', correct: false, index: 2}, 
       ]
     }, {
       text: "Do you need help with a webproject?",
       responses: [
-        {text: 'Hell yeah', correct: true}, 
-        {text: 'Maybe', correct: true}, 
-        {text: 'Nope', correct: false}, 
+        {text: 'Hell yeah', correct: true, index: 0}, 
+        {text: 'Maybe', correct: true, index: 1}, 
+        {text: 'Nope', correct: false, index: 2}, 
       ]
     }, 
     {
       text: "How many years experience should your candidate have at least?",
       responses: [
-        {text: '1 year', correct: true}, 
-        {text: '1 - 3 years', correct: true}, 
-        {text: '+3 years', correct: false}, 
+        {text: '1 year', correct: true, index: 0}, 
+        {text: '1 - 3 years', correct: true, index: 1}, 
+        {text: '+3 years', correct: false, index: 2}, 
       ]
     },
     {
       text: "Do you prefer to work with mentally strong people",
       responses: [
-        {text: 'Hell yeah', correct: true}, 
-        {text: 'Maybe', correct: true}, 
-        {text: 'Nope', correct: false}, 
+        {text: 'Hell yeah', correct: true, index: 0}, 
+        {text: 'Maybe', correct: true, index: 1}, 
+        {text: 'Nope', correct: false, index: 2}, 
       ]
     }, {
       text: "What do you think of working 9 to 5?",
       responses: [
-        {text: 'Exactly how i like it', correct: false}, 
-        {text: 'Thats old fashion', correct: true}, 
-        {text: 'Not enaugh hours', correct: true}, 
+        {text: 'Exactly how i like it', correct: false, index: 0}, 
+        {text: 'Thats old fashion', correct: true, index: 1}, 
+        {text: 'Not enaugh hours', correct: true, index: 2}, 
       ]
     }, 
   
@@ -123,7 +138,8 @@ export default {
        totalQuestions: 0,
        totalCorrectAnswers: 0,
        totalWrongAnswers: 0,
-       score: 0
+       score: 0,
+       UserAnswer: 0,
     }),
     methods: {
         next: function() {
@@ -132,9 +148,11 @@ export default {
             if (this.questionIndex === this.quiz.questions.length){
                 this.startQuiz = false;
             }
+            
         },
         // Go to previous question
         quizScore: function() {
+            
             this.totalQuestions = this.quiz.questions.length,
             this.score = (this.totalCorrectAnswers / this.totalQuestions) * 100
             
@@ -148,32 +166,88 @@ export default {
             this.totalCorrectAnswers = 0;
             this.totalWrongAnswers = 0;
         },
-        checkAnswer: function (correct){
+        checkAnswer: function (correct, index){
             this.clicked = true;
             if(correct){
                 console.log("helemaal goed");
                 this.totalCorrectAnswers++;
                 console.log("aantal goede antwoorden: " + this.totalCorrectAnswers);
+
             } else {    
                 console.log("helemaal fout");
                 this.totalWrongAnswers++;
                 console.log("aantal foute antwoorden: " + this.totalWrongAnswers);
+
+                
             }
             this.quizScore();
+            this.UserAnswer = index;
+      
+
         }
-    },
-    computed: {
-        compClasses: function() {
-            return {
-                'fa fa-check' : this.faCheck && !this.faTimes,
-                'fa fa-times' : this.faTimes && !this.faCheck
-            }
-        }
-    } 
+    }
 }
 </script>
 
 <style scoped>
 
+.absolute{
+position: absolute;
+
+}
+
+.relative{
+ position: relative;
+}
+
+/* Animation */
+
+@keyframes fadeInUp {
+    from {
+        transform: translate3d(0,40px,0)
+    }
+
+    to {
+        transform: translate3d(0,0,0);
+        opacity: 1
+    }
+}
+
+@-webkit-keyframes fadeInUp {
+    from {
+        transform: translate3d(0,40px,0)
+    }
+
+    to {
+        transform: translate3d(0,0,0);
+        opacity: 1
+    }
+}
+
+
+
+.animated {
+    animation-duration: 0.3s;
+    animation-fill-mode: both;
+    -webkit-animation-duration: 0.3s;
+    -webkit-animation-fill-mode: both;
+}
+
+.animatedEnd {
+    animation-duration: 2s;
+    animation-fill-mode: both;
+    -webkit-animation-duration: 2s;
+    -webkit-animation-fill-mode: both;
+}
+
+.animatedFadeInUp {
+    opacity: 0
+}
+
+.fadeInUp {
+    opacity: 0;
+    animation-name: fadeInUp;
+    -webkit-animation-name: fadeInUp;
+}
 
 </style>
